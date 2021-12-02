03b193c2d3a31fd815b29e5a98f83c74818892f5
Fabian Windheuser <fwindheuser@palantir.com>
2021-12-02 10:33:06 +0000

    Start publishing Gradle module metadata (#109)
    Start publishing Gradle module metadata

--- src/main/java/com/palantir/gradle/externalpublish/ExternalPublishBasePlugin.java
+++ src/main/java/com/palantir/gradle/externalpublish/ExternalPublishBasePlugin.java
@@ -33,7 +33,6 @@ import org.gradle.api.publish.PublishingExtension;
 import org.gradle.api.publish.maven.MavenPublication;
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
 import org.gradle.api.publish.maven.tasks.PublishToMavenRepository;
-import org.gradle.api.publish.tasks.GenerateModuleMetadata;
 import org.gradle.plugins.signing.SigningExtension;
 import org.gradle.plugins.signing.SigningPlugin;
 
@@ -50,7 +49,6 @@ final class ExternalPublishBasePlugin implements Plugin<Project> {
         applyPublishingPlugins();
         linkWithRootProject();
         disableOtherPublicationsFromPublishingToSonatype();
-        disableModuleMetadata();
 
         // Sonatype requires we set a description on the pom, but the maven plugin will overwrite anything we set on
         // pom object. So we set the description on the project if it is not set, which the maven plugin reads from.
@@ -104,13 +102,6 @@ final class ExternalPublishBasePlugin implements Plugin<Project> {
                 return true;
             });
         });
-    }
-
-    private void disableModuleMetadata() {
-        // Turning off module metadata so that all consumers just use regular POMs
-        project.getTasks()
-                .withType(GenerateModuleMetadata.class)
-                .configureEach(generateModuleMetadata -> generateModuleMetadata.setEnabled(false));
     }
 
     public void addPublication(String publicationName, Action<MavenPublication> publicationConfiguration) {
