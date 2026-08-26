516dc8f95be4a262fe04f98511a0cbd366e7c2bd
Finlay Williams <finlayw@palantir.com>
2026-08-26 14:43:30 +0100

    Support Gradle 9 for IntelliJ plugin publishing

--- src/test/groovy/com/palantir/gradle/externalpublish/ExternalPublishRootPluginIntegrationSpec.groovy
+++ src/test/groovy/com/palantir/gradle/externalpublish/ExternalPublishRootPluginIntegrationSpec.groovy
@@ -178,18 +178,40 @@ class ExternalPublishRootPluginIntegrationSpec extends IntegrationSpec {
         if (type == 'intellij') {
             // language=gradle
             subprojectBuildGradle << '''
-                intellij{
-                    pluginName = 'foo'
-                    updateSinceUntilBuild = true
-                    version = "2024.1"
-                    plugins = ['java', 'org.jetbrains.plugins.gradle']
+                repositories {
+                    intellijPlatform {
+                        defaultRepositories()
+                    }
                 }
-                
-                patchPluginXml {
-                    pluginDescription = "bar"
-                    sinceBuild = '213'
-                    untilBuild = ''
-                } 
+
+                dependencies {
+                    intellijPlatform {
+                        intellijIdeaCommunity('2024.1') {
+                            useInstaller = false
+                        }
+                        bundledPlugins('com.intellij.java', 'org.jetbrains.plugins.gradle')
+                    }
+                }
+
+                intellijPlatform {
+                    projectName = 'foo'
+                    pluginConfiguration {
+                        name = 'foo'
+                        description = 'bar'
+                        ideaVersion {
+                            sinceBuild = '213'
+                            untilBuild = ''
+                        }
+                    }
+                }
+            '''.stripIndent(true)
+
+            file('src/main/resources/META-INF/plugin.xml', subprojectDir) << '''
+                <idea-plugin>
+                    <id>com.palantir.externalpublish.test</id>
+                    <name>foo</name>
+                    <vendor>Palantir</vendor>
+                </idea-plugin>
             '''.stripIndent(true)
         }
 
