d04d3246b7562b115e844f4ff3d830e314906029
Finlay Williams <finlayw@palantir.com>
2026-08-27 15:06:16 +0100

    Test external publishing with Gradle 9-compatible dependencies

--- src/test/groovy/com/palantir/gradle/externalpublish/ExternalPublishRootPluginIntegrationSpec.groovy
+++ src/test/groovy/com/palantir/gradle/externalpublish/ExternalPublishRootPluginIntegrationSpec.groovy
@@ -54,7 +54,7 @@ class ExternalPublishRootPluginIntegrationSpec extends IntegrationSpec {
                 dependencies {
                     classpath 'com.gradle.publish:plugin-publish-plugin:1.3.0'
                     classpath 'com.palantir.gradle.conjure:gradle-conjure:5.51.0'
-                    classpath 'com.palantir.gradle.consistentversions:gradle-consistent-versions:2.26.0'
+                    classpath 'com.palantir.gradle.consistentversions:gradle-consistent-versions:3.18.0'
                 }
             }
 
